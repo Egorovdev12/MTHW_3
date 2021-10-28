65699cab250f9ffc6e7d8e3e0c12d9bab24704ff
ILYA EGOROV <egorovcf@gmail.com>
2021-10-28 14:45:57 +0500

    Commit #2

--- src/HW2/Buhgalteria.java
+++ src/HW2/Buhgalteria.java
@@ -1,14 +1,14 @@
 package HW2;
 
+import java.util.concurrent.atomic.LongAdder;
 
 public class Buhgalteria {
 
-    public void shop(long[] money) {
+    public void shop(long[] money, LongAdder adder) {
         long localSum = 0;
         for (long a : money) {
             localSum = localSum + a;
         }
-        Homework2.generalAdder.add(localSum);
+        adder.add(localSum);
     }
-
 }
